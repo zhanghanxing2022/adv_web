0e4948f5146afdea16e6f02c31e89003c2c10989
austin <1120194513@qq.com>
2023-06-15 09:05:22 +0800

    文案、解决抽搐、后退速度

--- src/assets/external/config.js
+++ src/assets/external/config.js
@@ -30,8 +30,7 @@ config.optionsMap = new Map([
     ]]
 ])
 
-const text1 = "这是冒泡排序"
-const text2 = ""
-const text3 = ""
-const text4 = ""
-config.readMe = [text1, text2, text3, text4];
+const text1 = "这是冒泡排序的示例，你可以在这里观察冒泡排序。点击右侧三个黄色按钮可进行交互:点按按钮next以执行下一步，点按按钮restart可重置当前序列到初试状态，点按按钮shuffle可随机生成一个新的序列。点击屏幕以关闭提示";
+const text2 = "这是二叉树遍历的示例，你可以在这里观察二叉树的前、中、后序遍历过程。点击前方五个黄色按钮可进行交互:点按midOrder/preOrder/postOrder按钮可切换到对应序遍历模式的初始状态，点按next按钮可以按照当前序进行下一步遍历，点按按钮shuffle可随机生成一颗新的二叉树。点击屏幕以关闭提示。";
+const text3 = "这是选择排序的示例，你可以在这里观察选择排序。点击右侧三个黄色按钮可进行交互:点按按钮next以执行下一步，点按按钮restart可重置当前序列到初试状态，点按按钮shuffle可随机生成一个新的序列。点击屏幕以关闭提示。";
+config.readMe = [text1, text2, text3];
